3b0f127c06e38b5a2f462af4691ad50028258393
Your Name <kenny130196@gmail.com>
2022-01-16 23:49:03 +0200

    //создаем переменную const  для получения слайдов через селектор const slides = document.querySelectorAll('.slide') //через цикл for проходим через каждый слайд for (const slide of slides) {     slide.addEventListener('click', () => {//создаем слушатель событий         clearActiveClasses() //уберает активность с предыдущего слайда         slide.classList.add('active')//включение анимации на слайде     }) }
    function clearActiveClasses() {//функция удаления активности со слайда
        slides.forEach((slide) => {
            slide.classList.remove('active')
        })
    }

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+//создаем переменную const  для получения слайдов через селектор
 const slides = document.querySelectorAll('.slide')
-
+//через цикл for проходим через каждый слайд
 for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
+    slide.addEventListener('click', () => {//создаем слушатель событий
+        clearActiveClasses() //уберает активность с предыдущего слайда
+        slide.classList.add('active')//включение анимации на слайде
     })
 }
 
-function clearActiveClasses() {
+function clearActiveClasses() {//функция удаления активности со слайда
     slides.forEach((slide) => {
         slide.classList.remove('active')
     })
